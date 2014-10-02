2be641720a7ef67862a7cd61ec2f4cbe68912cc7
michael-schmid <m.schmid06@googlemail.com>
2014-10-02 13:16:37 +0200

    adjusted data types for info attributes

--- DB/MyInfoDB/StructuredInformationStorage/tblI.sql
+++ DB/MyInfoDB/StructuredInformationStorage/tblI.sql
@@ -1,11 +1,11 @@
 ﻿CREATE TABLE [dbo].[tblI]
 (
-	[Id] INT NOT NULL identity PRIMARY KEY, 
-    [iDate] DATETIME NULL DEFAULT getdate(), 
-    [hid] hierarchyid(10) not null, 
-    [Level] AS hid.GetLevel(), 
-	[Name] VARCHAR(128) NULL, 
-    [Key] VARCHAR(100) NULL, 
-    [Value] NCHAR(1000) NULL, 
-    [Url] VARCHAR(100) NULL
+	[Id]		INT NOT NULL identity PRIMARY KEY, 
+    [iDate]		DATETIME NULL DEFAULT getdate(), 
+    [hid]		hierarchyid(10) not null, 
+    [Level]		AS hid.GetLevel(), 
+	[Name]		VARCHAR(128) NULL, 
+    [Key]		VARCHAR(100) NULL, 
+    [Value]		varchar(1000) NULL, 
+    [Url]		VARCHAR(2000) NULL
 )
