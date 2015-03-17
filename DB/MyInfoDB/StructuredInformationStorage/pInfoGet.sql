23f354eff3365475674878d9dee7b3f7d252ce11
michael-schmid <m.schmid06@googlemail.com>
2015-03-18 00:12:32 +0100

    enhaned pInfoGet with InfoJournal: view: list

--- DB/MyInfoDB/StructuredInformationStorage/pInfoGet.sql
+++ DB/MyInfoDB/StructuredInformationStorage/pInfoGet.sql
@@ -25,26 +25,50 @@ create procedure [dbo].[pInfoGet]
 	@Name			varchar(1000) = null		,	-- not yet implemented
 	@Key			varchar(1000) = null		,	-- not yet implemented
 	@Value			varchar(1000) = null		,	-- not yet implemented
-	@ParentId		int = null						-- not yet implemented
+	@ParentId		int			  = null		,	-- not yet implemented
+	@view			varchar(50) = 'hirar'
 as
 set nocount on
 
+	-- provide the last 50 inforamtion of a particular information node
+	if @view = 'hirar'
+		begin
+			select	top 50		
+					id							,
+					hid.ToString()		Hierarchy	,
+					null				Path		,	-- to be implemented : node/node/node
+					Level						,
+					replicate('     ', hid.GetLevel()) + Name as VisualPath  ,
+					Name						,
+					[Key]						,
+					Value						,
+					Url							,
+					convert(varchar(20), iDate, 120)	iDate
+			from	tbli
+			where	(id = @id
+				or	@id is null)	
+			order	by hid 
+		end
 
-	select	top 50		
-			id							,
-			hid.ToString()		Hierarchy	,
-			null				Path		,	-- to be implemented : node/node/node
-			Level						,
-			replicate('     ', hid.GetLevel()) + Name as VisualPath  ,
-			Name						,
-			[Key]						,
-			Value						,
-			Url							,
-			convert(varchar(20), iDate, 120)	iDate
-	from	tbli
-	where	(id = @id
-		or	@id is null)	
-	order	by hid 
+	-- return a plain list of information (sorted iDate desc)
+	-- to display the journal of added information
+	if @view = 'list'
+		begin
+			
+			select	top 50		
+					id							,
+					hid.ToString()		Hierarchy	,
+					null				Path		,	-- to be implemented : node/node/node
+					Level						,
+					replicate('     ', hid.GetLevel()) + Name as VisualPath  ,
+					Name						,
+					[Key]						,
+					Value						,
+					Url							,
+					convert(varchar(20), iDate, 120)	iDate
+			from	tbli
+			order	by iDate Desc
+		end
 
 
 	
